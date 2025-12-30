d51f0ce92baeb34c19e6388059427cec414e2416
IMTewari <89448558+IMTewari@users.noreply.github.com>
2025-12-30 01:51:21 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true }
+  experimental: { appDir: true },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Mark optional native deps of ws as externals to avoid bundling errors
+      config.externals = config.externals || [];
+      config.externals.push('utf-8-validate', 'bufferutil');
+    }
+    return config;
+  }
 }
 module.exports = nextConfig
